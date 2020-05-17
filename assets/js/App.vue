9b782134104a190370399ef833bccba70e879ce6
Tom Meyers <tom@odex.be>
2020-05-17 15:17:32 +0200

    Fixed typo

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -73,7 +73,7 @@ export default {
       link: [
         {
           rel: "icon",
-          href: require("@/images/tos.svg"),
+          href: require("@/images/tosicon.svg"),
           sizes: "any",
           type: "image/svg+xml"
         }
